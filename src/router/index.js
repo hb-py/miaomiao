92cdb8ba857b8145d7a2927f21e232c528cb7bb7
hb-py <878403168@qq.com>
2020-07-16 14:44:46 +0800

    zujian

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import MoiveRouter from './moive'
+import MineRouter from './mine'
+import CinemaRouter from './cinema'
 
 Vue.use(VueRouter)
 
 const routes = [
+  MoiveRouter,
+  MineRouter,
+  CinemaRouter,
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/*',
+    redirect: '/movie'
   }
 ]
 
